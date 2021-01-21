77f4d75403c3385a06edb1992732fd81b04b1b55
Jennifer Reyna <jennifer.m.reyna@outlook.com>
2021-01-21 12:51:13 -0600

    added comments

---
+++ PyBank/main.py
@@ -0,0 +1,30 @@
+#Dependencies
+
+import pandas as pd
+import os
+import csv
+
+#Create path for CSV
+
+budget_path = "Resources/budget_data.csv"
+
+#Read CSV File
+budget_path = os.path.join("Resources/budget_data.csv")
+
+budget_path_df.head()
+
+#Set Variables
+
+#Determing total number of months in the dataset 
+
+
+
+#Determine net total amount of "Profit/Loss" over the entire period
+
+#Calculate the changes in "Profit/Loss" over the entire period, then find the average of those changes
+
+#Determine greatest increase in profits (date and amount) over entire period
+
+#Determine greatest decrease in losses (date and amount) over entire period
+
+#Print your final analysis 
